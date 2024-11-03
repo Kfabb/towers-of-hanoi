39ccab25836898fb2237dec283ad99d6297d99a8
Kathryn Fabbroni <kathrynfabbroni@kathryns-air-3.mynetworksettings.com>
2024-11-03 14:03:22 -0500

    added conditional to check if disc is the last disc on the indicated peg

--- main.js
+++ main.js
@@ -19,9 +19,9 @@ function moveDisc(disc, peg){
     peg = currentBoard[0];
     if (peg[peg.length-1] > disc || peg.length === 0){
       peg.push(disc);
-      if (currentBoard[1].includes(disc)){
+      if (currentBoard[1].includes(disc) && currentBoard[1][currentBoard[1].length-1] === disc){
         currentBoard[1].pop(disc)
-      } else if (currentBoard[2].includes(disc)){
+      } else if (currentBoard[2].includes(disc) && currentBoard[2][currentBoard[2].length - 1 === disc]){
         currentBoard[2].pop(disc)
       } 
       } else (console.log("discs cannot be placed on smaller discs")) } 
@@ -29,18 +29,18 @@ else if (peg === 2){
         peg = currentBoard[1];
         if (peg[peg.length-1] > disc || peg.length === 0){
           peg.push(disc);
-          if (currentBoard[0].includes(disc)){
+          if (currentBoard[0].includes(disc) && currentBoard[0][currentBoard[0].length -1]===disc){
             currentBoard[0].pop(disc)
-          } else if (currentBoard[2].includes(disc)){
+          } else if (currentBoard[2].includes(disc) && currentBoard[2][currentBoard[2].length-1] === 0){
             currentBoard[2].pop(disc);}} else (console.log("discs cannot be placed on smaller discs"))
       }
 else if (peg === 3){
   peg = currentBoard[2];
   if (peg[peg.length-1] > disc || peg.length === 0){
     peg.push(disc);
-    if (currentBoard[1].includes(disc)){
+    if (currentBoard[1].includes(disc) && currentBoard[1][currentBoard[1].length-1]===0){
       currentBoard[1].pop(disc);
-    } else if (currentBoard[0].includes(disc)){
+    } else if (currentBoard[0].includes(disc) && currentBoard[0][currentBoard[0].length-1] === 0){
      currentBoard[0].pop(disc)
     }
   } else (console.log("discs cannot be placed on smaller discs"))
@@ -65,44 +65,44 @@ function checkWinner(){
   
 }
 
-// showBoard()
-// moveDisc(1,3)
-// moveDisc(2,2)
-// moveDisc(1,2)
-// moveDisc(3,1)
-// moveDisc(3,3)
-// moveDisc(1,1)
-// moveDisc(2,3)
-// moveDisc(1,3)
-// moveDisc(4,2)
-// moveDisc(1,2)
-// moveDisc(2,1)
-// moveDisc(1,1)
-// moveDisc(3,2)
-// moveDisc(1,3)
-// moveDisc(2,2)
-// moveDisc(1,2)
-// moveDisc(5,3)
-// moveDisc(1,3)
-// moveDisc(2,1)
-// moveDisc(1,1)
-// moveDisc(3,3)
-// moveDisc(1,3)
-// moveDisc(2,2)
-// moveDisc(1,2)
-// moveDisc(3,1)
-// moveDisc(1,3)
-// moveDisc(2,1)
-// moveDisc(1,1)
-// moveDisc(4,3)
-// moveDisc(1,3)
-// moveDisc(2,2)
-// moveDisc(1,2)
-// moveDisc(3,3)
-// moveDisc(1,1)
-// moveDisc(2,3)
-// moveDisc(1,3)
-// checkWinner()
+showBoard()
+moveDisc(1,3)
+moveDisc(2,2)
+moveDisc(1,2)
+moveDisc(3,1)
+moveDisc(3,3)
+moveDisc(1,1)
+moveDisc(2,3)
+moveDisc(1,3)
+moveDisc(4,2)
+moveDisc(1,2)
+moveDisc(2,1)
+moveDisc(1,1)
+moveDisc(3,2)
+moveDisc(1,3)
+moveDisc(2,2)
+moveDisc(1,2)
+moveDisc(5,3)
+moveDisc(1,3)
+moveDisc(2,1)
+moveDisc(1,1)
+moveDisc(3,3)
+moveDisc(1,3)
+moveDisc(2,2)
+moveDisc(1,2)
+moveDisc(3,1)
+moveDisc(1,3)
+moveDisc(2,1)
+moveDisc(1,1)
+moveDisc(4,3)
+moveDisc(1,3)
+moveDisc(2,2)
+moveDisc(1,2)
+moveDisc(3,3)
+moveDisc(1,1)
+moveDisc(2,3)
+moveDisc(1,3)
+checkWinner()
 
 //get number to move
 //get peg to move to
