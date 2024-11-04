0de17a8c7984dc4b57e03ec02b7110eef8550d5e
Kathryn Fabbroni <kathrynfabbroni@kathryns-air-3.mynetworksettings.com>
2024-11-04 18:52:47 -0500

    fixed what i thought was right but wasn'fixed the checkwinner function

--- main.js
+++ main.js
@@ -15,61 +15,85 @@ newGame()
 
 function moveDisc(disc, peg){
   // let lastPeg = peg[peg.length-1];
+  let lastDiscPeg1 = currentBoard[0][currentBoard[0].length-1];
+  let lastDiscPeg2 = currentBoard[1][currentBoard[1].length-1];
+  let lastDiscPeg3 = currentBoard[2][currentBoard[2].length-1];
+  let largeOnSmall = "Sorry, Charlie, you can't place larger discs on top of smaller discs. Try again!";
+  let notLastDisc = "Sorry, Charlie, you can only move the last disc on a peg! Try again"
   if (peg === 1){
     peg = currentBoard[0];
     if (peg[peg.length-1] > disc || peg.length === 0){
-      peg.push(disc);
-      if (currentBoard[1].includes(disc) && currentBoard[1][currentBoard[1].length-1] === disc){
-        currentBoard[1].pop(disc)
-      } else if (currentBoard[2].includes(disc) && currentBoard[2][currentBoard[2].length - 1 === disc]){
-        currentBoard[2].pop(disc)
-      } 
-      } else (console.log("discs cannot be placed on smaller discs")) } 
+      // peg.push(disc);
+      if (currentBoard[1].includes(disc)){
+        if (lastDiscPeg2 === disc)
+        {currentBoard[1].pop(disc);
+          peg.push(disc)
+      }} else if (currentBoard[2].includes(disc)){
+        if (lastDiscPeg3 === disc){
+        currentBoard[2].pop(disc);
+        peg.push(disc)
+       } else {console.log(notLastDisc)}
+      } else (console.log(largeOnSmall)) } }
 else if (peg === 2){
         peg = currentBoard[1];
         if (peg[peg.length-1] > disc || peg.length === 0){
-          peg.push(disc);
-          if (currentBoard[0].includes(disc) && currentBoard[0][currentBoard[0].length -1]===disc){
-            currentBoard[0].pop(disc)
-          } else if (currentBoard[2].includes(disc) && currentBoard[2][currentBoard[2].length-1] === 0){
-            currentBoard[2].pop(disc);}} else (console.log("discs cannot be placed on smaller discs"))
+          // peg.push(disc);
+          if (currentBoard[0].includes(disc)){
+            if (lastDiscPeg1 === disc){
+            currentBoard[0].pop(disc);
+          peg.push(disc)} else {console.log(notLastDisc)}
+          } else if (currentBoard[2].includes(disc))
+            if (lastDiscPeg3 === disc){
+            currentBoard[2].pop(disc);
+          peg.push(disc)} else {console.log(notLastDisc)}}
       }
 else if (peg === 3){
   peg = currentBoard[2];
   if (peg[peg.length-1] > disc || peg.length === 0){
-    peg.push(disc);
-    if (currentBoard[1].includes(disc) && currentBoard[1][currentBoard[1].length-1]===0){
-      currentBoard[1].pop(disc);
-    } else if (currentBoard[0].includes(disc) && currentBoard[0][currentBoard[0].length-1] === 0){
-     currentBoard[0].pop(disc)
+    // peg.push(disc);
+    if (currentBoard[1].includes(disc) && lastDiscPeg2 === disc){
+      if (lastDiscPeg2 === disc)
+      {currentBoard[1].pop(disc);
+      peg.push(disc);} else  {console.log(notLastDisc)}
+    } 
+    else if (currentBoard[0].includes(disc)){
+      if(lastDiscPeg1 === disc)
+     {currentBoard[0].pop(disc);
+     peg.push(disc)} else {console.log(notLastDisc)}
     }
-  } else (console.log("discs cannot be placed on smaller discs"))
+  } else (console.log(largeOnSmall))
 }
-
 
 showBoard()     
 }
  
 function checkWinner(){
   //check that the discs are bag in order on a new peg 
-  if (currentBoard[1] = [5,4,3,2,1]){
-    console.log("The Player has won the game! Let's go again!");
-    newGame();
-  showBoard();
-  }
-  else if (currentBoard[2] = [5,4,3,2,1]){
-    console.log("The Player has won the game. Let's go again!");
-    newGame();
-  showBoard();
-  } else {console.log("Sorry, Charlie, you havn'et won yet:(")}
-  
+  let peg2 = currentBoard[1];
+  let peg3 = currentBoard[2];
+  if (peg2.length === 5){
+    if (peg2[0] === 5 && peg2[peg2.length -1] === 1){
+      console.log("You've won! Let's go again!");
+      newGame();
+      showBoard();
+    }
+  } else if (peg3.length === 5){
+    if (peg3[0] === 5 && peg3[peg3.length -1] === 1){
+      console.log("You've won! Let's go again!");
+      newGame();
+      showBoard();
+    }
+  } else {
+    console.log("Sorry, Charlie, you haven't won yet.")
+  } 
 }
+
+
 
 showBoard()
 moveDisc(1,3)
 moveDisc(2,2)
 moveDisc(1,2)
-moveDisc(3,1)
 moveDisc(3,3)
 moveDisc(1,1)
 moveDisc(2,3)
@@ -83,15 +107,12 @@ moveDisc(1,3)
 moveDisc(2,2)
 moveDisc(1,2)
 moveDisc(5,3)
-moveDisc(1,3)
-moveDisc(2,1)
 moveDisc(1,1)
-moveDisc(3,3)
+moveDisc(2,3)
 moveDisc(1,3)
-moveDisc(2,2)
-moveDisc(1,2)
 moveDisc(3,1)
-moveDisc(1,3)
+moveDisc(4,3)
+moveDisc(1,2)
 moveDisc(2,1)
 moveDisc(1,1)
 moveDisc(4,3)
@@ -101,8 +122,8 @@ moveDisc(1,2)
 moveDisc(3,3)
 moveDisc(1,1)
 moveDisc(2,3)
+moveDisc(1,1)
 moveDisc(1,3)
-checkWinner()
 
 //get number to move
 //get peg to move to
